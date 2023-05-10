e14c85621978a8251a3bb51976bbfa420a562245
joost de keijzer <j@dkzr.nl>
2023-05-10 16:30:07 +0200

    add link to Plugin list text

--- eurofxref.php
+++ eurofxref.php
@@ -2,7 +2,8 @@
 /**
  * Plugin Name: Euro FxRef Currency Converter
  * Plugin URI: https://wordpress.org/plugins/euro-fxref-currency-converter/
- * Description: Adds the [currency] and [currency_legal] shortcodes to convert currencies based on the ECB reference exchange rates. Please visit https://wordpress.org/plugins/euro-fxref-currency-converter/ for help and options.
+ * Description: Adds the [currency] and [currency_legal] shortcodes to convert currencies based on the ECB reference exchange rates. Please visit <a href="https://wordpress.org/plugins/euro-fxref-currency-converter/" target="_blank">the plugin page on WordPress.org</a> for help and options.
+ * Text Domain: euro-fxref-currency-converter
  * Version: 2.0
  * Author: Joost de Keijzer
  * Author URI: https://dkzr.nl/
